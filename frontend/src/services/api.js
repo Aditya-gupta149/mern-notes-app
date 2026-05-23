b482d157c08da829f3c594dc1235ad639e8d598c
Aditya-gupta149 <159017148+Aditya-gupta149@users.noreply.github.com>
2026-05-23 15:44:19 +0530

    Fixed API route

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-notes-backend-1upw.onrender.com",
+  baseURL: "https://mern-notes-backend-1upw.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
