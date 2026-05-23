7e6a149b9b562bbebb476b6a271dfec380948d4f
Aditya-gupta149 <159017148+Aditya-gupta149@users.noreply.github.com>
2026-05-23 14:54:44 +0530

    Connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mern-notes-backend-1upw.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
